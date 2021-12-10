d9e55f8937bf40115d11f270202b162618bc46f3
Soheil Rahmat <soheilrt314@gmail.com>
2021-12-11 00:04:49 +0330

    Replace punctuations in text with white-space before tokenizing

--- tokenizers/word_tokenizer.py
+++ tokenizers/word_tokenizer.py
@@ -1,9 +1,15 @@
 from typing import List
+
 from nltk import word_tokenize
 
 from models.tokenizer import Tokenizer
 
 
 class WordTokenizer(Tokenizer):
+    __punctuations: str = '!"#$%&\'()*+,./:;<=>?@[\\]^_-`{|}~'
+
     def tokenize(self, text) -> List[str]:
-        return word_tokenize(text)
+        return word_tokenize(self.__translate_punctuations_to_space(text))
+
+    def __translate_punctuations_to_space(self, text):
+        return text.translate(str.maketrans(self.__punctuations, ' ' * len(self.__punctuations)))
