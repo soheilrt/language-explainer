1c0f5eb128f693214e0452523f966e8ea5a3e31a
Soheil Rahmat <soheilrt314@gmail.com>
2021-12-11 00:03:49 +0330

    Add DefinerStorage Model

--- models/definer.py
+++ models/definer.py
@@ -12,3 +12,13 @@ class Definer(ABC):
     @abstractmethod
     def define(self, word) -> List[WordDefinition]:
         pass
+
+
+class DefinerStorage(Definer, ABC):
+    @abstractmethod
+    def store(self, word: str, definitions: List[WordDefinition]):
+        pass
+
+    @abstractmethod
+    def exists(self, word: str) -> bool:
+        pass
