a349a358ed5b40e568ea75e7e9494de0f3f90c61
Soheil Rahmat <soheilrt314@gmail.com>
2021-12-12 17:34:19 +0330

    Update explain file

--- explain.py
+++ explain.py
@@ -24,41 +24,61 @@ from writers.json_writer import JsonFileWriter
 
 
 def get_arguments() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description='Do you want an explain for hard in a text or audio file?')
+    parser = argparse.ArgumentParser(description='Do you want an explain for hard word in a text or audio file?')
     parser.add_argument('-i', '--input', help='the file to explain', type=str)
     parser.add_argument(
-        '-t', '--input-type', help='the file type', type=str, choices=['text', 'url', 'pdf'], defualt='text'
+        '-t', '--input-type', help='the file type, Default = text', type=str,
+        choices=['text', 'url', 'pdf'], default='text'
     )
     parser.add_argument(
-        '--pdf-engine', help='the pdf engine', type=str, choices=['pdfminer', 'pyPdf'], default='pdfminer'
+        '--pdf-engine', help='the pdf engine, Default = pdfminer', type=str,
+        choices=['pdfminer', 'pyPdf'], default='pdfminer'
     )
     parser.add_argument('-o', '--output', help='the output file', type=str)
-    parser.add_argument('-ot', '--output-type', help='the output file type', type=str, choices=['json', 'csv'])
     parser.add_argument(
-        '--cefr-level', help='the cefr level', type=str, choices=['A1', 'A2', 'B1', 'B2', 'C1', 'C2'], default='B1'
+        '-ot', '--output-type', help='the output file type, Default = json', type=str,
+        choices=['json', 'csv'], default='json'
     )
-    parser.add_argument('--ignore-unknown-cefr', help='ignore words with unknown cefr level', type=bool, default=False)
+    parser.add_argument(
+        '--cefr-level', help='minimum word\'s cefr level to consider, default = B1', type=str,
+        choices=['A1', 'A2', 'B1', 'B2', 'C1', 'C2'], default='B1'
+    )
+    parser.add_argument(
+        '--ignore-unknown-cefr', help='ignore words with unknown cefr level, default = False',
+        default=False, action='store_true'
+    )
+    parser.add_argument(
+        '-v', '--verbose', help='verbose mode', default=False, action='store_true'
+    )
     args = parser.parse_args()
     return args
 
 
 def get_reader(args: argparse.Namespace) -> Reader:
+    print("reading from {}".format(args.input))
     if args.input_type == 'text':
+        print('Using TextReader')
         return TextReader(args.input)
     elif args.input_type == 'url':
+        print('Using URLReader')
         return URLReader(args.input)
-    elif args.input_type == 'pdf' and args.pdf_engines == 'pdfminer':
+    elif args.input_type == 'pdf' and args.pdf_engine == 'pdfminer':
+        print('Using PDFMinerReader')
         return PDFMinerReader(file_path=args.input)
-    elif args.input_type == 'pdf' and args.pdf_engines == 'pyPdf':
+    elif args.input_type == 'pdf' and args.pdf_engine == 'pyPdf':
+        print('Using PyPDFReader')
         return PyPDFReader(file_path=args.input)
 
     raise Exception('Unknown input type')
 
 
 def get_writer(args: argparse.Namespace) -> Writer:
+    print("writing to {}".format(args.output))
     if args.output_type == 'json':
+        print('output file type is json')
         return JsonFileWriter(args.output)
     if args.output_type == 'csv':
+        print('output file type is csv')
         return CSVWriter(args.output)
 
     raise Exception('Unknown output type')
@@ -86,14 +106,23 @@ def get_definer(_: argparse.Namespace) -> Definer:
     )
 
 
+def get_definition_composer(args: argparse.Namespace) -> DefinitionComposer:
+    return DefinitionComposer(
+        tokenizer=get_tokenizer(args),
+        middlewares=get_middlewares(args),
+        definer=get_definer(args),
+        verbose=args.verbose
+    )
+
+
 def main():
     args = get_arguments()
     reader = get_reader(args)
     writer = get_writer(args)
-    tokenizer = get_tokenizer(args)
-    middlewares = get_middlewares(args)
-    definer = get_definer(args)
-    composer = DefinitionComposer(tokenizer=tokenizer, middlewares=middlewares, definer=definer)
 
+    composer = get_definition_composer(args)
     composer.compose_write(reader=reader, writer=writer)
 
+
+if __name__ == '__main__':
+    main()
